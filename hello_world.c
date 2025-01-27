b664c3775e6350e281d76b434de441ff146d50cd
Léo LEESCO <leo.leesco@gmail.com>
2025-01-27 23:51:44 +0100

    use openmp

--- hello_world.c
+++ hello_world.c
@@ -2,12 +2,9 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  MPI_Init(NULL, NULL);
 
-  int rank, size;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-  printf("hello from thread %d/%d", rank, size);
-  return EXIT_SUCCESS;
+#pragma omp parallel
+  printf("hello from thread %d/%d", omp_get_thread_num(),
+         omp_get_num_threads());
+  return 0;
 }
