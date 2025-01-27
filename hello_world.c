9f1acf603747325c661bdd124810089641c34e33
Léo LEESCO <leo.leesco@gmail.com>
2025-01-28 00:09:10 +0100

    format

--- hello_world.c
+++ hello_world.c
@@ -4,8 +4,9 @@
 int main(int argc, char *argv[]) {
 
 #pragma omp parallel
-	{	
-  printf("hello from thread %d/%d\n", omp_get_thread_num(),
-         omp_get_num_threads());}
+  {
+    printf("hello from thread %d/%d\n", omp_get_thread_num(),
+           omp_get_num_threads());
+  }
   return 0;
 }
